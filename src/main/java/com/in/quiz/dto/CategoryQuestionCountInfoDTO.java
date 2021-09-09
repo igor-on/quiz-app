c5a6cc23e4a698a089b70fbeaa92eaab03e1aaf4
Igor Nowakowski <igornowakowski24@wp.pl>
2021-09-09 11:34:20 +0200

    Added CategoryQuestionCount info class

---
+++ src/main/java/com/in/quiz/dto/CategoryQuestionCountInfoDTO.java
@@ -0,0 +1,47 @@
+package com.in.quiz.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.in.quiz.frontend.Difficulty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@ToString
+public class CategoryQuestionCountInfoDTO {
+
+    @JsonProperty("category_id")
+    private int categoryId;
+    private CategoryQuestionCountInfo categoryQuestionCountInfo;
+
+    @NoArgsConstructor
+    @Getter
+    @ToString
+    public static class CategoryQuestionCountInfo {
+
+        @JsonProperty("total_question_count")
+        private int totalQuestionCount;
+        @JsonProperty("total_easy_question_count")
+        private int totalEasyQuestionCount;
+        @JsonProperty("total_medium_question_count")
+        private int totalMediumQuestionCount;
+        @JsonProperty("total_hard_question_count")
+        private int totalHardQuestionCount;
+    }
+
+    public int getTotalQuestionsCount() {
+        return categoryQuestionCountInfo.totalQuestionCount;
+    }
+
+    public int getQuestionCountForDifficulty(Difficulty difficulty) {
+        switch (difficulty) {
+            case EASY:
+                return categoryQuestionCountInfo.totalEasyQuestionCount;
+            case MEDIUM:
+                return categoryQuestionCountInfo.totalMediumQuestionCount;
+            case HARD:
+                return categoryQuestionCountInfo.totalHardQuestionCount;
+        }
+        return 0;
+    }
+}
--- src/main/java/com/in/quiz/service/QuizDataService.java
+++ src/main/java/com/in/quiz/service/QuizDataService.java
@@ -1,7 +1,9 @@
 package com.in.quiz.service;
 
 import com.in.quiz.dto.CategoriesDTO;
+import com.in.quiz.dto.CategoryQuestionCountInfoDTO;
 import com.in.quiz.dto.QuestionsDTO;
+import com.in.quiz.frontend.Difficulty;
 import com.in.quiz.frontend.GameOptions;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
@@ -24,17 +26,29 @@ public class QuizDataService {
         return result.getCategories();
     }
 
+    private CategoryQuestionCountInfoDTO getCategoryQuestionCount(int categoryId) {
+        URI uri = UriComponentsBuilder.fromHttpUrl("https://opentdb.com/api_count.php")
+                .queryParam("category", categoryId)
+                .build().toUri();
+        log.info("Quiz category question count retrieve URL: " + uri);
+        CategoryQuestionCountInfoDTO result = restTemplate.getForObject(uri, CategoryQuestionCountInfoDTO.class);
+        log.info("Quiz category question count content: " + result);
+        return result;
+    }
+
     public List<QuestionsDTO.QuestionDTO> getQuizQuestions(GameOptions gameOptions) {
+        return getQuizQuestions(gameOptions.getNumberOfQuestions(), gameOptions.getCategoryId(), gameOptions.getDifficulty());
+    }
+
+    private List<QuestionsDTO.QuestionDTO> getQuizQuestions(int numberOfQuestions, int categoryId, Difficulty difficulty) {
         URI uri = UriComponentsBuilder.fromHttpUrl("https://opentdb.com/api.php")
-                .queryParam("amount", gameOptions.getNumberOfQuestions())
-                .queryParam("category", gameOptions.getCategoryId())
-                .queryParam("difficulty", gameOptions.getDifficulty().name().toLowerCase(Locale.ROOT))
+                .queryParam("amount", numberOfQuestions)
+                .queryParam("category", categoryId)
+                .queryParam("difficulty", difficulty.name().toLowerCase())
                 .build().toUri();
         log.info("Quiz question retrieve URL: " + uri);
-
         QuestionsDTO result = restTemplate.getForObject(uri, QuestionsDTO.class);
-        log.info("Quiz questions: " + result.getResults());
-
+        log.info("Quiz questions: Open Trivia DB response code = " + result.getResponseCode() + ". Content: " + result.getResults());
         return result.getResults();
     }
 }
